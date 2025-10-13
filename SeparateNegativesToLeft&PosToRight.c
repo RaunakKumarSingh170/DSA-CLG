ec80e946820d8cfe37a0434cd8469f9d0234332b
Raunak Kumar Singh <raunaksingh2904@gmail.com>
2025-10-13 15:35:10 +0530

    dynamic memory allocation

---
+++ SeparateNegativesToLeft&PosToRight.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int arr[] = {-3,2,4,-7,-1,1,5};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int negat[3];
+    int posi[4];
+    int ans[7];
+    for(int i=0;i<7;i++){
+        if(arr[i]<0){
+            negat[j] = arr[i];
+            j++;
+        }
+        else{
+            posi[k] = arr[i];
+            k++;
+        }
+    }
+}
